b68afefc65596f86868091c1a25e6e47d92d6138
daniel-tomiwa <danielgbenga814@gmail.com>
2023-06-30 03:48:39 +0000

    add more configuration files for app updates

--- payload_request.sh
+++ payload_request.sh
@@ -2,4 +2,4 @@
 
 curl -d '{"PULocationID": 10, "DOLocationID": 50, "trip_distance": 40}' \
      -H "Content-Type: application/json" \
-     -X POST http://192.168.253.68:8000/predict
+     -X POST http://192.168.253.101:8000/predict
