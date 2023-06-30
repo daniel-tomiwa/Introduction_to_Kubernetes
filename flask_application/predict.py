c68abc43a391e7fde3ae3979e97002d55b2b168d
daniel-tomiwa <danielgbenga814@gmail.com>
2023-06-30 03:52:43 +0000

    update script to load model from volume

--- flask_application/predict.py
+++ flask_application/predict.py
@@ -2,9 +2,14 @@ import pickle
 import os
 from flask import Flask, request, jsonify
 
-with open('lin_reg.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
 
+path_to_volume_specified_model = "/mnt/lin_reg.bin"
+if os.path.isfile(path_to_volume_specified_model):
+    with open(path_to_volume_specified_model, 'rb') as f_in:
+        dv, model = pickle.load(f_in)
+else:
+    print("The model was not found! Can't make any preidctions")
+    
 def preprocess(ride):
     features = {}
     features['PU_DO'] = f"{ride['PULocationID']}_{ride['DOLocationID']}"
@@ -41,4 +46,5 @@ def predict_endpoint():
     return jsonify(prediction_json)
 
 if __name__=="__main__":
+        
     app.run(debug=True, host="0.0.0.0", port=8000)
